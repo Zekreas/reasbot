65f487c9a66755d13069d19c3d76646f2a641eba
Zekreas <progamers551712@gmail.com>
2025-09-20 14:12:29 +0300

    helpguncelleme5

--- bot.py
+++ bot.py
@@ -346,11 +346,13 @@ async def on_ready():
 def kanalbulunamadi(ctx):
     return ctx.send("Kanal bulunamadı. Lütfen geçerli bir kanal ID'si girin.")
 
-@bot.check
-async def globally_block_dms(ctx):
-    if ctx.command.name == 'help':
-        return await bot.is_owner(ctx.author)
-    return True
+@bot.command(name='help')
+async def help_command(ctx):
+    if not await bot.is_owner(ctx.author):
+        return
+    
+    # Default help işlevselliği
+    await ctx.send_help()
 
 @tasks.loop(seconds=30)
 async def gununhantigonder():
